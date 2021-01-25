665c6bb9661e7f46bdaffc8d430868c9acff81eb
bongofury <jtroutmanman@gmail.com>
2021-01-24 21:38:20 -0600

    added create_table methods to create_users and create_movies migrations

--- db/migrate/20210125031136_create_movies.rb
+++ db/migrate/20210125031136_create_movies.rb
@@ -1,4 +1,11 @@
 class CreateMovies < ActiveRecord::Migration
   def change
+    create_table :movies do |t|
+      t.string :name
+      t.string :description
+      t.integer :year
+      t.integer :director_id
+      t.integer :user_id
+    end
   end
 end
