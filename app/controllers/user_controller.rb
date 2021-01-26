d6d60fe4212c4f7b9b452a9e195c64bb33f34f87
bongofury <jtroutmanman@gmail.com>
2021-01-26 00:10:35 -0600

    removed password parameter from User.find_by method call and added authenticate method to if statement

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -17,8 +17,8 @@ class UserController < ApplicationController
     end
 
     post '/user' do
-        @user = User.find_by(username: params[:username], password: params[:password])
-        if @user
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
             redirect '/user'
         else
