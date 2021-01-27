4088ef5dcf890f50b8cb7adc8fc955224d1c2ae6
bongofury <jtroutmanman@gmail.com>
2021-01-27 16:30:12 -0600

    added get request for genres/:name in genre controller. created show.erb view for this route.

--- app/controllers/genre_controller.rb
+++ app/controllers/genre_controller.rb
@@ -10,5 +10,16 @@ class GenreController < ApplicationController
         end
     end
 
+    get '/genres/:name' do
+        @user = User.find_by_id(session[:user_id])
+        if @user
+            @genre = Genre.find_by(name: params[:name])
+            @list = @user.movies.select {|movie| movie.genres.include?(@genre)}.sort_by {|movie| movie.name}
+            erb :"/genres/show"
+        else
+            erb :"/users/error"
+        end
+    end
+
 
 end
