6388bd22d0cbd66784a953a5b9c2dcc5b4d0d28d
bongofury <jtroutmanman@gmail.com>
2021-01-27 16:03:23 -0600

    changed get /genres path to gather Genre names from Genre.all method instead of user instance.genres method.

--- app/controllers/genre_controller.rb
+++ app/controllers/genre_controller.rb
@@ -3,7 +3,7 @@ class GenreController < ApplicationController
     get '/genres' do
         @user = User.find_by_id(session[:user_id])
         if @user
-            @genres = @user.genres.sort_by {|genre| genre.name}
+            @genres = Genre.all.sort_by {|genre| genre.name}
             erb :"/genres/index"
         else
             erb :"/users/error"
