76c1b9b9566e51af39b57bb421bdb2ca4ae57fb6
bongofury <jtroutmanman@gmail.com>
2021-01-27 01:49:48 -0600

    fixed links in user home page. created get route for genres index page and created view for genre index page.

--- app/controllers/genre_controller.rb
+++ app/controllers/genre_controller.rb
@@ -1,2 +1,14 @@
 class GenreController < ApplicationController
+
+    get '/genres' do
+        @user = User.find_by_id(session[:user_id])
+        if @user
+            @genres = @user.genres.sort_by {|genre| genre.name}
+            erb :"/genres/index"
+        else
+            erb :"/users/error"
+        end
+    end
+
+
 end
