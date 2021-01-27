1fc3786b9531ce8627ff00d4ce0122559377d185
bongofury <jtroutmanman@gmail.com>
2021-01-27 16:42:20 -0600

    added get request for formats/:name route. created show.erb view for formats.

--- app/controllers/format_controller.rb
+++ app/controllers/format_controller.rb
@@ -1,2 +1,24 @@
 class FormatController < ApplicationController
+
+    get '/formats' do
+        @user = User.find_by_id(session[:user_id])
+        if @user
+            @formats = Format.all.sort_by {|format| format.name}
+            erb :"/formats/index"
+        else
+            erb :"/users/error"
+        end
+    end
+
+    get '/formats/:name' do
+        @user = User.find_by_id(session[:user_id])
+        if @user
+            @format = Format.find_by(name: params[:name])
+            @list = @user.movies.select {|movie| movie.formats.include?(@format)}.sort_by {|movie| movie.name}
+            erb :"/formats/show"
+        else
+            erb :"/users/error"
+        end
+    end
+
 end
