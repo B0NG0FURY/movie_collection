ca2948965de5c7e8225a084143c66548d5572c6c
bongofury <jtroutmanman@gmail.com>
2021-02-01 15:40:29 -0600

    changed background color to a brighter yellow. changed seeds file so that multiple instances of same genre cannot be created.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 genres = ["action", "drama", "comedy", "horror", "sci-fi", "romance"]
 formats = ["blu-ray", "dvd", "vhs", "laserdisc"]
 
-genres.each {|genre| Genre.create(name: genre)}
-formats.each {|format| Format.create(name: format)}
+genres.each {|genre| Genre.find_or_create_by(name: genre)}
+formats.each {|format| Format.find_or_create_by(name: format)}
